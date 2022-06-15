adf52a39af533db92bb9d98b5f8e7d6be7603869
Ashik Reji <ashikreji27@gmail.com>
2022-06-14 22:55:12 -0400

    Completed 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 
@@ -22,6 +23,28 @@ test('renders content', () => {
   expect(blogLikes).toBeNull()
   expect(blogTitle).toHaveStyle('display: block')
   expect(blogUrl).toBeNull()
+})
+
+test('test for when button is clicked', async () => {
+  const blog = {
+    likes: 0,
+    author: 'Me',
+    title: 'Test Blog',
+    url: 'ref4rfg34f.com'
+  }
+
+  const { container } = render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const blogLikes = container.querySelector('.likes-container')
+  const blogUrl = container.querySelector('.url-container')
+
+  expect(blogLikes).not.toHaveStyle('display: none')
+  expect(blogUrl).not.toHaveStyle('display: none')
+
 })
 
 //CI=true npm test
